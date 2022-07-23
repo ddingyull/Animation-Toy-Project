4a369dd17aa475c958d0b8f67f555c415e5d13b1
yurim <dding.yull@gmail.com>
2022-07-24 01:28:16 +0900

    알고리즘 과제

--- algorithm.js/algorithm_homework.js
+++ algorithm.js/algorithm_homework.js
@@ -82,59 +82,168 @@ console.log(array);
   array.forEach(num2 => {arrSum3 += num2});
   console.log(arrSum3);
 
-  //4안) reduce
-  
+  //4안) reduce 메소드 활용
+  //피드백 : .reduce(리턴값, 배열의요소들)이기 때문에 따로 +수식을 써주지 않고 바로 return에 작성가능
+  let arrSum4 = array.reduce((arrSum4, num) => {
+    return arrSum4 + num
+  })
+  console.log(arrSum4);
+
+
+
+
 
   //동일한 요소same, 다른요소만diff배열 만들기
+  let fruits1 = ["사과", "딸기", "파인애플", "수박", "참외", "오렌지", "자두", "망고"];
+  let fruits2 = ["수박", "사과", "참외", "오렌지", "파인애플", "망고"];
+
+  let same = [];
+  let diff = [];
+
+  same = fruits1.filter((item) => fruits2.includes(item));
+  diff = fruits1.filter((item) => !fruits2.includes(item));
+
+  console.log(same);
+  console.log(diff);
+  
+
+
+
 
   //글자 수가 6글자인 것 찾아서 반환
+  const fruits = ["Apple", "Banana", "Cherry!"];
+
+  let words = [];
+  words = fruits.filter(item => item.length === 6);
+  console.log(words);
+
+
+
+
+
 
   //평균구하기
+  function solution(arr){
+    let sum = 0;
+    for(let i = 0; i<arr.length; i++){
+      sum += arr[i];
+      let average = sum / arr.length;
+    }
+    return average;
+  }
+
+
+
+
 
   //번호 가리기
-//   //1안 ) for문
-// function solution(phone_number) {
-//   let blind = '';
-//   for(let i = 0; i<phone_number.length; i++){
-//     if(i< phone_number.length-4)
-//   }
-// }
-//   //2안 ) for of문
-// function solution(phone_number) {
-//   var answer = '';
-//   let a = 0;  
+  function solution(phone_number) {
+    let blind = '';
+    for(let i = 0; i<phone_number.length; i++){
+      if(i< phone_number.length-4){
+        blind += '*';
+      } else{
+        blind +=phone_number[i]
+      }
+    }
+    return blind;
+  }
 
-//   for(let i of phone_number){
-//       a++
-//       if(a < phone_number.length-3) answer += '*';
-//       else answer+= i;
-//   }
 
-//   return answer;
-// }
+
+
+
 //자릿수 더하기
+//피드백 : 문자열로 바꿔줄 때는 N.toString()형태로 바꿔준다
+function solution(N) {
+  let jWord = N.toString();
+  let answer = 0;
+  for(let i = 0; i<jWord.length; i++){
+    answer += Number(jWord[i]);
+  }
+  return answer;
+}
 
-//모의 고사
+
+
+
+//모의 고사 : 아래 코드인데 계속 정답이 아니라고나옴.. 
+// let answers = [1,2,3,4,5,2,3,4,5,5]
+
+function solution(answers) {
+  //최종적으로 구해야하는 
+  let answer = [];
+  //수포자의 답을 각각의 변수이름으로 정의하기
+  const answer1 = [1,2,3,4,5];
+  const answer2 = [2,1,2,3,2,4,2,5];
+  const answer3 = [3,3,1,1,2,2,4,4,5,5];
+  //각 수포자가 맞힌 정답의 개수를 넣어줄 배열 만들기(단, 3명이니깐 index 3개로 만들기)
+  let score = [0,0,0];
+
+  //answers.length는 문제의 개수이다 각 문제별로 수포자들의 답이 같다면 score를 높여준다
+  for(let i = 0; i<answers.length; i++){
+    if(answers[i] === answer1([i%answer1.length])){score[0]++};
+    if(answers[i] === answer2([i%answer2.length])){score[1]++};
+    if(answers[i] === answer3([i%answer3.length])){score[2]++};
+  }
+  
+
+  //가장 높은 score를 구한다 : Math.max(...이름) -> 가장 큰 숫자를 구하되 배열 형태를 의미함
+  const maxScore = Math.max(...score);
+
+
+  /*score.length라고 작성하긴하지만 .. 그냥 수포자 3명의 각 점수를 for문으로 돌려서 
+    maxScor값과 동일한 값을 answer함수에 넣어준다 
+    이때, answer.push(i+1)는 index는 0부터 시작하기때문에 1을 더해서 표현한다 */
+  for(let i = 0; i<score.length; i++){
+    return maxScore === score[i] ? 'answer.push(i+1)' : ''
+  };
+
+  //answer라고 선언한 배열에 maxScore값을 return한다 
+  return answer;
+}
+//최종적으로 가장 많은 문제를 맞힌 사람이 누군지 배열에 담아 return하는 함수
+console.log(solution(answers));
+
+
+
+
+
 
 //없는 숫자 더하기
+// let number = [1,2,3,4,5,6,7,8,9]
 
-//나누어 떨어지는 숫자 배열
+function solution(numbers) {
+  //없는 숫자들의 합을 0으로 선언
+  let emptySum = 0;
+
+  //피드백 : i<numbers.length로 선언하면 모두 확인할 수 없음, 9까지 점검하기 위해 10으로 간단히 해결가능
+  for(let i = 0; i<10; i++){
+    if(!number.includes(i)) {emptySum += i}; 
+  }
+  return emptySum;
+}
+
+
+
+
+//나누어 떨어지는 숫자 : 배열 아래 코드인데 계속 정답이 아니라고나옴.. 
+function solution(arr, divisor) {
+  var answer = [];
+
+  for(let i= 0; i<arr.length; i++){
+    arr[i] % divisor === 0 ? 'answer.push(arr[i])' : '-1';
+  }
+  answer.sort((a,b) => a - b);
+  return answer;
+}
 
 
 
 
 
-let numbers = [1, 2, 3, 4, 5, 6];
-let fruits = ["사과", "바나나", "수박", "포도", "파인애플"];
 
-numbers.forEach(function (number, index, array) {
-  console.log(number, index, array);
-});
 
-fruits.forEach((fruit, i, arr) => {
-    console.log(fruit, i, arr);
-  });
-  
 
 
 
